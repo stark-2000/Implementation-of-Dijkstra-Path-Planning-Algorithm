a723ce337e7f098b97a48fcf0e8b145c97163d72
stark-2000 <dhineshrajasekaran@gmail.com>
2023-03-11 05:47:23 -0500

    Implemented 8 action set for robot

--- dijkstra_Dhinesh_Rajasekaran.py
+++ dijkstra_Dhinesh_Rajasekaran.py
@@ -1,39 +1,57 @@
 import time
+import os
 
 
 
 class action_set():
-
     def __init__(self):
+        print("8 Action Set: UP, DOWN, LEFT, RIGHT, Top_Right, Top_Left, Bottom_Right, Bottom_Left")
 
     #Actions Set = {(1,0), (-1,0), (0,1), (0,-1), (1,1), (-1,1), (1,-1), (-1,-1)}
+    def up(self, node): #(0,1)
+        node_op = (node[0], node[1]+1)
+        return node_op
 
-    def up(self): #(0,1)
+    def down(self, node): #(0,-1)
+        node_op = (node[0], node[1]-1)
+        return node_op
 
-    def down(self): #(0,-1)
+    def left(self, node): #(-1,0)
+        node_op = (node[0]-1, node[1])
+        return node_op
 
-    def left(self): #(-1,0)
+    def right(self, node): #(1,0)
+        node_op = (node[0]+1, node[1])
+        return node_op
 
-    def right(self): #(1,0)
+    def up_right(self, node): #(1,1)
+        node_op = (node[0]+1, node[1]+1)
+        return node_op
 
-    def TR(self): #(1,1)
+    def up_left(self, node): #(-1,1)
+        node_op = (node[0]-1, node[1]+1)
+        return node_op
 
-    def TL(self): #(-1,1)
+    def down_right(self, node): #(1,-1)
+        node_op = (node[0]+1, node[1]-1)
+        return node_op
 
-    def BR(self): #(1,-1)
+    def down_left(self, node): #(-1,-1)
+        node_op = (node[0]-1, node[1]-1)
+        return node_op
 
-    def BL(self): #(-1,-1)
 
-
-
+p1 = action_set()
 class dijkstra_algo():
-    def __init__(self)
+    def __init__(self):
     
-    def check_if_obstacle(self):
+    def check_if_obstacle(self, node):
+        return 1
+        return 0
 
-    def check_if_goal_node(self):
-    return True
-    return False
+    def check_if_goal_node(self, node):
+        return True
+        return False
     
     def graph_search(self):
 
@@ -41,24 +59,39 @@ class dijkstra_algo():
         return path #start to goal
     
 
-class visualization()
+class visualization():
     def __init__(self):
 
-    def GUI_opencv(self):
+    def GUI_opencv(self): #node exploration & optimal path
 
     
-    
+
+p2 = dijkstra_algo()
+p3 = visualization()
 def main():
-    start = time.time()
+    start_time = time.time()
+
+    print("Enter Start Node: ")
+    start_node = input()
+
+    print("Enter Goal Node: ")
+    goal_node = input()
+
+    if p2.check_if_obstacle(start_node):
+        print("\nInvalid Start Node.. Re-Enter the Start node: ")
+
+    if p2.check_if_obstacle(goal_node):
+        print("\nInvalid Goal Node.. Re-Enter the Goal node: ")
+        
 
 
-    end = time.time()
-    processing_time = end - start
+    end_time = time.time()
+    processing_time = end_time - start_time
     print("\nTime taken to find Optimal Path: ", processing_time)
 
     
 
-    
 
-        
-    
+
+if __name__ == '__main__':
+    main()
